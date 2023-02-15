47a2ea6a4563d998f4167e818e64df75726aa25e
Adefeso Qudus Adebola || Qdus <adefesoq@gmail.com>
2023-02-15 02:02:24 +0100

    added vercel analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,13 @@
 import "@/styles/globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
+      <Analytics />
     </QueryClientProvider>
   );
 }
